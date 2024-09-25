8d4e23cc83af99733f1fbbb053f7bfe08d632e72
AkhilReddy505 <reddyakhil959@gmail.com>
2024-09-25 12:36:32 +0530

    Add files via upload

---
+++ multi.dart
@@ -0,0 +1,9 @@
+var element = {
+  'title': 'Apple and more more thing',
+  'category': 'Fruit',
+  'description': 'Some data to describe',
+  'price': 24.67,
+  'bonus': '1% bonus',
+  'group': 'Picnik',
+  'step': 1.0
+};
